847d426ed20a987ceb8dff71fd5e1f97b43e784c
cc <caijianbang@cdeledu.com>
2021-02-24 10:20:56 +0800

    components(demo)

---
+++ src/components/Home/Test01.jsx
@@ -0,0 +1,11 @@
+import  React, { Component } from 'react';
+
+export default class Test01 extends Component{
+  render() {
+    return (
+      <React.Fragment>
+        This is component for Test01.
+      </React.Fragment>
+    )
+  }
+}
---
+++ src/components/Home/Test02.jsx
@@ -0,0 +1,11 @@
+import  React, { Component } from 'react';
+
+export default class Test02 extends Component{
+  render() {
+    return (
+      <React.Fragment>
+        This is component for Test02.
+      </React.Fragment>
+    )
+  }
+}
